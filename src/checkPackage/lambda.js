322c29fbdf6ed3033563b9db0b58d73a5df76395
tresvitae <tresvitae@hotmail.com>
2020-03-14 13:22:39 +0100

    Funkcja Lambda pobierajaca informacje o paczce

--- src/checkPackage/lambda.js
+++ src/checkPackage/lambda.js
@@ -1,11 +1,17 @@
-const provider = require('./fetchPackageInfo')
+// const provider = require('./fetchPackageInfo')
+const provider = require('./helper')
 
 module.exports.main = async (event, context) => {
-  console.log(event)
-  const { packageId, company } = event.body
+  // console.log(event)
+  // const { packageId, company } = event.body
   
-  const result = await provider.fetchPackageInfo(packageId, company)
-  console.log(JSON.stringify(result));
+  // const result = await provider.fetchPackageInfo(packageId, company)
+  // console.log(JSON.stringify(result));
 
-  return result
+  // return result
+
+  console.log(event)
+  const response = { message: helper('World') };
+
+  return response
 }
