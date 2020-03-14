2549358e0defcf3295e6fd7e9593c65e454c7264
tresvitae <tresvitae@hotmail.com>
2020-03-14 18:31:57 +0100

    Set us-west-2 region

--- src/checkPackage/lambda.js
+++ src/checkPackage/lambda.js
@@ -1,17 +1,11 @@
-// const provider = require('./fetchPackageInfo')
-const provider = require('./helper')
+const provider = require('./fetchPackageInfo')
 
 module.exports.main = async (event, context) => {
-  // console.log(event)
-  // const { packageId, company } = event.body
-  
-  // const result = await provider.fetchPackageInfo(packageId, company)
-  // console.log(JSON.stringify(result));
-
-  // return result
-
   console.log(event)
-  const response = { message: helper('World') };
+  const { packageId, company } = event.body
+  
+  const result = await provider.fetchPackageInfo(packageId, company)
+  console.log(JSON.stringify(result));
 
-  return response
+  return result
 }
